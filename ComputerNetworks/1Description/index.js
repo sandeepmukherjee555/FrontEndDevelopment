70f8230f2e4b6226934012dcb038406947a96c78
sandeepmukherjee <sandeepmukherjee12@gmail.com>
2024-12-09 22:17:26 +0530

    Computer Networks demo

---
+++ ComputerNetworks/1Description/index.js
@@ -0,0 +1,10 @@
+/*
+Computer Networks
+
+Network - It is a group or system of interconnected people or items.
+Computer connected with each other with cables or wireless(Wi-Fi)
+is called Computer Networks
+
+Internet - Internet is a network of Computer Networks 
+
+*/
